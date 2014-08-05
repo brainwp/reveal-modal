508fe6c9f0d46bb851177e3a7bf19ac5ffd7071b
matheus <contato@matheusgimenez.com.br>
2014-08-05 16:16:51 -0300

    javascript quase la

--- reveal-modal.php
+++ reveal-modal.php
@@ -39,6 +39,12 @@ class Reveal_Modal_Plugin
 		$this->admin_init(); //call function to init metabox and options
 		register_activation_hook(__FILE__, array($this, 'install')); //call function to install the plugin
 		register_uninstall_hook(__FILE__, array($this, 'uninstall')); //call function to remove the plugin
+		//var option
+		$this->option_string = get_option('reveal-modal-string-random');
+		add_action('wp_head', array($this, 'add_metatags')); //add meta tags
+		add_action('wp_head', array($this, 'scripts')); //add javascript & css
+		add_action('wp_footer', array($this, 'footer')); //add javascript & css
+
 	}
 
 	private function admin_init()
@@ -61,17 +67,53 @@ class Reveal_Modal_Plugin
 			$index = mt_rand(0, $validCharNumber - 1);
 			$result .= $validCharacters[$index];
 		}
-		return '-'.$result;
+		return '-' . $result;
 	}
 
 	public function install()
 	{
-		add_option( 'reveal-modal-string-random', $this->random_string(), '', 'yes' );
+		delete_option('reveal-modal-string-random');
+		add_option('reveal-modal-string-random', $this->random_string(), '', 'yes');
 	}
-	public function uninstall(){
-		if ( !defined( 'WP_UNINSTALL_PLUGIN' ) )
+
+	public function uninstall()
+	{
+		if (!defined('WP_UNINSTALL_PLUGIN'))
 			exit();
-		delete_option( 'reveal-modal-string-random' );
+		delete_option('reveal-modal-string-random');
+	}
+
+	public function add_metatags()
+	{
+		echo '<meta name="reveal-modal-cfg-str" content="' . $this->option_string . '">';
+	}
+
+	public function scripts()
+	{
+		wp_enqueue_style('reveal-modal-css', plugin_dir_url(__FILE__) . 'assets/css/foundation.min.css');
+		wp_enqueue_script(
+			'reveal-modal-js-foundation',
+			plugin_dir_url(__FILE__) . 'assets/js/foundation.js',
+			array('jquery'),
+			'',
+			true
+		);
+
+		wp_enqueue_script(
+			'reveal-modal-js',
+			plugin_dir_url(__FILE__) . 'assets/js/reveal.js',
+			array('jquery'),
+			'',
+			true
+		);
+	}
+
+	public function footer()
+	{
+		echo '<div id="reveal-modal-id" class="reveal-modal" data-reveal>';
+		echo '<a class="close-reveal-modal">&#215;</a>';
+		echo '</div>';
 	}
 }
+
 new Reveal_Modal_Plugin();
